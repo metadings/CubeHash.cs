7deb76d4b12db61ceb46ccfcc8fe4ffa587cb5b1
metadings <metadings@live.de>
2016-08-19 18:24:55 +0200

    Updating CubeHash.cs

--- CubeHash.cs
+++ CubeHash.cs
@@ -1,15 +1,10 @@
 /*	CubeHash.cs source code package - C# implementation
 
-	Written in 2016 by Uli Riehm <metadings@live.de>
+	2016-08-19
+	Uli Riehm <metadings@live.de>
+	Public Domain
 
-	To the extent possible under law, the author(s) have dedicated all copyright
-	and related and neighboring rights to this software to the public domain
-	worldwide. This software is distributed without any warranty.
-
-	You should have received a copy of the CC0 Public Domain Dedication along with
-	this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-*/
-/*	Based on supercop-20141124/crypto_hash/cubehash512/unrolled
+	Based on supercop-20141124/crypto_hash/cubehash512/unrolled
 	     and supercop-20141124/crypto_hash/cubehash512/unrolled3
 
 	20100623, 20100917
@@ -167,7 +162,7 @@ namespace Crypto
 
 			buffer[bufferFilled++] = 0x80;
 			for (int i = bufferFilled; i < buffer.Length; ++i) buffer[i] = 0x00;
-			TransformBlock();
+			TransformBlock(buffer, 0);
 
 			state[31] ^= 1;
 
--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 **﻿﻿CubeHash.cs source code package - C# implementation**
 
 ```
-Written in 2016 by Uli Riehm <metadings@live.de>
-
-To the extent possible under law, the author(s) have dedicated all copyright
-and related and neighboring rights to this software to the public domain
-worldwide. This software is distributed without any warranty.
-
-You should have received a copy of the CC0 Public Domain Dedication along with
-this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+2016-08-19
+Uli Riehm <metadings@live.de>
+Public Domain
 
 Based on supercop-20141124/crypto_hash/cubehash512/unrolled
      and supercop-20141124/crypto_hash/cubehash512/unrolled3
