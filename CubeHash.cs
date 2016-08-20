4058544178ec403819920df3407925c872d8321a
metadings <metadings@live.de>
2016-08-20 03:13:28 +0200

    Updating CubeHash.cs

--- CubeHash.cs
+++ CubeHash.cs
@@ -51,11 +51,11 @@ namespace Crypto
 
 		public const int ROUNDS = 16;
 
-		private readonly byte[] buffer = new byte[BlockSizeInBytes * 16];
+		private readonly byte[] buffer = new byte[512];
 
 		private int bufferFilled;
 
-		private readonly uint[] state = new uint[BlockSizeInBytes];
+		// private readonly uint[] state = new uint[BlockSizeInBytes];
 
 		public CubeHash() : this(512) { }
 
@@ -71,11 +71,11 @@ namespace Crypto
 
 		public override void Initialize()
 		{
-			Clear();
+			HashClear();
 
-			state[00] = (uint)HashSize / 8;
-			state[01] = BlockSizeInBytes;
-			state[02] = ROUNDS;
+			x00 = (uint)HashSize / 8;
+			x01 = BlockSizeInBytes;
+			x02 = ROUNDS;
 
 			TransformBlock();
 
@@ -88,7 +88,21 @@ namespace Crypto
 		{
 			_isInitialized = false;
 
-			for (int i = 0; i < 32; ++i) state[i] = 0U;
+			x00 = 0U; x01 = 0U; x02 = 0U; x03 = 0U;
+			x04 = 0U; x05 = 0U; x06 = 0U; x07 = 0U;
+			x08 = 0U; x09 = 0U; x0A = 0U; x0B = 0U;
+			x0C = 0U; x0D = 0U; x0E = 0U; x0F = 0U;
+
+			x10 = 0U; x11 = 0U; x12 = 0U; x13 = 0U;
+			x14 = 0U; x15 = 0U; x16 = 0U; x17 = 0U;
+			x18 = 0U; x19 = 0U; x1A = 0U; x1B = 0U;
+			x1C = 0U; x1D = 0U; x1E = 0U; x1F = 0U;
+
+			y0 = 0U; y1 = 0U; y2 = 0U; y3 = 0U;
+			y4 = 0U; y5 = 0U; y6 = 0U; y7 = 0U;
+			y8 = 0U; y9 = 0U; yA = 0U; yB = 0U;
+			yC = 0U; yD = 0U; yE = 0U; yF = 0U;
+
 			for (int i = 0; i < buffer.Length; ++i) buffer[i] = 0x00;
 			bufferFilled = 0;
 		}
@@ -146,7 +160,7 @@ namespace Crypto
 
 		public virtual byte[] Final()
 		{
-			var result = new byte[(int)HashSize / 8];
+			var result = new byte[HashSize / 8];
 			Final(result);
 			return result;
 		}
@@ -161,16 +175,77 @@ namespace Crypto
 	state->x[state->pos / 32] ^= u; /**/
 
 			buffer[bufferFilled++] = 0x80;
-			for (int i = bufferFilled; i < buffer.Length; ++i) buffer[i] = 0x00;
+			for (int i = bufferFilled; i < BlockSizeInBytes; ++i) buffer[i] = 0x00;
 			TransformBlock(buffer, 0);
 
-			state[31] ^= 1;
+			x1F ^= 1;
 
 			TransformBlock();
 			TransformBlock();
 
 			// if (BitConverter.IsLittleEndian)
-			Buffer.BlockCopy(state, 0, result, 0, (int)HashSize / 8);
+
+			// for (int i = 0; i < (HashSize / 8); ++i) result[i] = 0x00;
+
+			switch (HashSize)
+			{
+				case 224:
+					UInt32ToBytes(x00, result, 0);
+					UInt32ToBytes(x01, result, 4);
+					UInt32ToBytes(x02, result, 8);
+					UInt32ToBytes(x03, result, 12);
+					UInt32ToBytes(x04, result, 16);
+					UInt32ToBytes(x05, result, 20);
+					UInt32ToBytes(x06, result, 24);
+					break;
+
+				case 256:
+					UInt32ToBytes(x00, result, 0);
+					UInt32ToBytes(x01, result, 4);
+					UInt32ToBytes(x02, result, 8);
+					UInt32ToBytes(x03, result, 12);
+					UInt32ToBytes(x04, result, 16);
+					UInt32ToBytes(x05, result, 20);
+					UInt32ToBytes(x06, result, 24);
+					UInt32ToBytes(x07, result, 28);
+					break;
+
+				case 384:
+					UInt32ToBytes(x00, result, 0);
+					UInt32ToBytes(x01, result, 4);
+					UInt32ToBytes(x02, result, 8);
+					UInt32ToBytes(x03, result, 12);
+					UInt32ToBytes(x04, result, 16);
+					UInt32ToBytes(x05, result, 20);
+					UInt32ToBytes(x06, result, 24);
+					UInt32ToBytes(x07, result, 28);
+					UInt32ToBytes(x08, result, 32);
+					UInt32ToBytes(x09, result, 36);
+					UInt32ToBytes(x0A, result, 40);
+					UInt32ToBytes(x0B, result, 44);
+					break;
+
+				case 512:
+					UInt32ToBytes(x00, result, 0);
+					UInt32ToBytes(x01, result, 4);
+					UInt32ToBytes(x02, result, 8);
+					UInt32ToBytes(x03, result, 12);
+					UInt32ToBytes(x04, result, 16);
+					UInt32ToBytes(x05, result, 20);
+					UInt32ToBytes(x06, result, 24);
+					UInt32ToBytes(x07, result, 28);
+					UInt32ToBytes(x08, result, 32);
+					UInt32ToBytes(x09, result, 36);
+					UInt32ToBytes(x0A, result, 40);
+					UInt32ToBytes(x0B, result, 44);
+					UInt32ToBytes(x0C, result, 48);
+					UInt32ToBytes(x0D, result, 52);
+					UInt32ToBytes(x0E, result, 56);
+					UInt32ToBytes(x0F, result, 60);
+					break;
+
+				// default: throw new InvalidOperationException();
+			}
 
 			_isInitialized = false;
 		}
@@ -197,289 +272,234 @@ namespace Crypto
 			TransformBlock(null, 0);
 		}
 
+		uint x00, x01, x02, x03, x04, x05, x06, x07;
+		uint x08, x09, x0A, x0B, x0C, x0D, x0E, x0F;
+		uint x10, x11, x12, x13, x14, x15, x16, x17;
+		uint x18, x19, x1A, x1B, x1C, x1D, x1E, x1F;
+
+		uint y0, y1, y2, y3, y4, y5, y6, y7;
+		uint y8, y9, yA, yB, yC, yD, yE, yF;
+
 		protected virtual void TransformBlock(byte[] data, int start)
 		{
 			if (data != null)
 			{
-				for (int i = 0; i < (BlockSizeInBytes / 4); i++)
-					state[i] ^= data[start + i];
+				x00 ^= BytesToUInt32(data, start);
+				x01 ^= BytesToUInt32(data, start + 4);
+				x02 ^= BytesToUInt32(data, start + 8);
+				x03 ^= BytesToUInt32(data, start + 12);
+				x04 ^= BytesToUInt32(data, start + 16);
+				x05 ^= BytesToUInt32(data, start + 20);
+				x06 ^= BytesToUInt32(data, start + 24);
+				x07 ^= BytesToUInt32(data, start + 28);
 			}
-
-			uint state00 = state[0];
-			uint state01 = state[1];
-			uint state02 = state[2];
-			uint state03 = state[3];
-			uint state04 = state[4];
-			uint state05 = state[5];
-			uint state06 = state[6];
-			uint state07 = state[7];
-			uint state08 = state[8];
-			uint state09 = state[9];
-			uint state0A = state[10];
-			uint state0B = state[11];
-			uint state0C = state[12];
-			uint state0D = state[13];
-			uint state0E = state[14];
-			uint state0F = state[15];
-			uint state10 = state[16];
-			uint state11 = state[17];
-			uint state12 = state[18];
-			uint state13 = state[19];
-			uint state14 = state[20];
-			uint state15 = state[21];
-			uint state16 = state[22];
-			uint state17 = state[23];
-			uint state18 = state[24];
-			uint state19 = state[25];
-			uint state1A = state[26];
-			uint state1B = state[27];
-			uint state1C = state[28];
-			uint state1D = state[29];
-			uint state1E = state[30];
-			uint state1F = state[31];
-
-			uint y0, y1, y2, y3, y4, y5, y6, y7;
-			uint y8, y9, yA, yB, yC, yD, yE, yF;
 
 			for (int r = 0; r < ROUNDS; ++r)
 			{
-				state10 += state00;
-				state11 += state01;
-				state12 += state02;
-				state13 += state03;
-				state14 += state04;
-				state15 += state05;
-				state16 += state06;
-				state17 += state07;
-				state18 += state08;
-				state19 += state09;
-				state1A += state0A;
-				state1B += state0B;
-				state1C += state0C;
-				state1D += state0D;
-				state1E += state0E;
-				state1F += state0F;
+				x10 += x00;
+				x11 += x01;
+				x12 += x02;
+				x13 += x03;
+				x14 += x04;
+				x15 += x05;
+				x16 += x06;
+				x17 += x07;
+				x18 += x08;
+				x19 += x09;
+				x1A += x0A;
+				x1B += x0B;
+				x1C += x0C;
+				x1D += x0D;
+				x1E += x0E;
+				x1F += x0F;
 
-				y8 = state00;
-				y9 = state01;
-				yA = state02;
-				yB = state03;
-				yC = state04;
-				yD = state05;
-				yE = state06;
-				yF = state07;
-				y0 = state08;
-				y1 = state09;
-				y2 = state0A;
-				y3 = state0B;
-				y4 = state0C;
-				y5 = state0D;
-				y6 = state0E;
-				y7 = state0F;
+				y8 = x00;
+				y9 = x01;
+				yA = x02;
+				yB = x03;
+				yC = x04;
+				yD = x05;
+				yE = x06;
+				yF = x07;
+				y0 = x08;
+				y1 = x09;
+				y2 = x0A;
+				y3 = x0B;
+				y4 = x0C;
+				y5 = x0D;
+				y6 = x0E;
+				y7 = x0F;
 
-				state00 = ((y0 << 7) | (y0 >> (32 - 7))); // ROTATE(y0, 7);
-				state01 = ((y1 << 7) | (y1 >> (32 - 7))); // ROTATE(y1, 7);
-				state02 = ((y2 << 7) | (y2 >> (32 - 7))); // ROTATE(y2, 7);
-				state03 = ((y3 << 7) | (y3 >> (32 - 7))); // ROTATE(y3, 7);
-				state04 = ((y4 << 7) | (y4 >> (32 - 7))); // ROTATE(y4, 7);
-				state05 = ((y5 << 7) | (y5 >> (32 - 7))); // ROTATE(y5, 7);
-				state06 = ((y6 << 7) | (y6 >> (32 - 7))); // ROTATE(y6, 7);
-				state07 = ((y7 << 7) | (y7 >> (32 - 7))); // ROTATE(y7, 7);
-				state08 = ((y8 << 7) | (y8 >> (32 - 7))); // ROTATE(y8, 7);
-				state09 = ((y9 << 7) | (y9 >> (32 - 7))); // ROTATE(y9, 7);
-				state0A = ((yA << 7) | (yA >> (32 - 7))); // ROTATE(yA, 7);
-				state0B = ((yB << 7) | (yB >> (32 - 7))); // ROTATE(yB, 7);
-				state0C = ((yC << 7) | (yC >> (32 - 7))); // ROTATE(yC, 7);
-				state0D = ((yD << 7) | (yD >> (32 - 7))); // ROTATE(yD, 7);
-				state0E = ((yE << 7) | (yE >> (32 - 7))); // ROTATE(yE, 7);
-				state0F = ((yF << 7) | (yF >> (32 - 7))); // ROTATE(yF, 7);
+				x00 = ((y0 << 7) | (y0 >> (32 - 7))); // ROTATE(y0, 7);
+				x01 = ((y1 << 7) | (y1 >> (32 - 7))); // ROTATE(y1, 7);
+				x02 = ((y2 << 7) | (y2 >> (32 - 7))); // ROTATE(y2, 7);
+				x03 = ((y3 << 7) | (y3 >> (32 - 7))); // ROTATE(y3, 7);
+				x04 = ((y4 << 7) | (y4 >> (32 - 7))); // ROTATE(y4, 7);
+				x05 = ((y5 << 7) | (y5 >> (32 - 7))); // ROTATE(y5, 7);
+				x06 = ((y6 << 7) | (y6 >> (32 - 7))); // ROTATE(y6, 7);
+				x07 = ((y7 << 7) | (y7 >> (32 - 7))); // ROTATE(y7, 7);
+				x08 = ((y8 << 7) | (y8 >> (32 - 7))); // ROTATE(y8, 7);
+				x09 = ((y9 << 7) | (y9 >> (32 - 7))); // ROTATE(y9, 7);
+				x0A = ((yA << 7) | (yA >> (32 - 7))); // ROTATE(yA, 7);
+				x0B = ((yB << 7) | (yB >> (32 - 7))); // ROTATE(yB, 7);
+				x0C = ((yC << 7) | (yC >> (32 - 7))); // ROTATE(yC, 7);
+				x0D = ((yD << 7) | (yD >> (32 - 7))); // ROTATE(yD, 7);
+				x0E = ((yE << 7) | (yE >> (32 - 7))); // ROTATE(yE, 7);
+				x0F = ((yF << 7) | (yF >> (32 - 7))); // ROTATE(yF, 7);
 
-				state00 ^= state10;
-				state01 ^= state11;
-				state02 ^= state12;
-				state03 ^= state13;
-				state04 ^= state14;
-				state05 ^= state15;
-				state06 ^= state16;
-				state07 ^= state17;
-				state08 ^= state18;
-				state09 ^= state19;
-				state0A ^= state1A;
-				state0B ^= state1B;
-				state0C ^= state1C;
-				state0D ^= state1D;
-				state0E ^= state1E;
-				state0F ^= state1F;
+				x00 ^= x10;
+				x01 ^= x11;
+				x02 ^= x12;
+				x03 ^= x13;
+				x04 ^= x14;
+				x05 ^= x15;
+				x06 ^= x16;
+				x07 ^= x17;
+				x08 ^= x18;
+				x09 ^= x19;
+				x0A ^= x1A;
+				x0B ^= x1B;
+				x0C ^= x1C;
+				x0D ^= x1D;
+				x0E ^= x1E;
+				x0F ^= x1F;
 
-				y2 = state10;
-				y3 = state11;
-				y0 = state12;
-				y1 = state13;
-				y6 = state14;
-				y7 = state15;
-				y4 = state16;
-				y5 = state17;
-				yA = state18;
-				yB = state19;
-				y8 = state1A;
-				y9 = state1B;
-				yE = state1C;
-				yF = state1D;
-				yC = state1E;
-				yD = state1F;
+				y2 = x10;
+				y3 = x11;
+				y0 = x12;
+				y1 = x13;
+				y6 = x14;
+				y7 = x15;
+				y4 = x16;
+				y5 = x17;
+				yA = x18;
+				yB = x19;
+				y8 = x1A;
+				y9 = x1B;
+				yE = x1C;
+				yF = x1D;
+				yC = x1E;
+				yD = x1F;
 
-				state10 = y0;
-				state11 = y1;
-				state12 = y2;
-				state13 = y3;
-				state14 = y4;
-				state15 = y5;
-				state16 = y6;
-				state17 = y7;
-				state18 = y8;
-				state19 = y9;
-				state1A = yA;
-				state1B = yB;
-				state1C = yC;
-				state1D = yD;
-				state1E = yE;
-				state1F = yF;
+				x10 = y0;
+				x11 = y1;
+				x12 = y2;
+				x13 = y3;
+				x14 = y4;
+				x15 = y5;
+				x16 = y6;
+				x17 = y7;
+				x18 = y8;
+				x19 = y9;
+				x1A = yA;
+				x1B = yB;
+				x1C = yC;
+				x1D = yD;
+				x1E = yE;
+				x1F = yF;
 
-				state10 += state00;
-				state11 += state01;
-				state12 += state02;
-				state13 += state03;
-				state14 += state04;
-				state15 += state05;
-				state16 += state06;
-				state17 += state07;
-				state18 += state08;
-				state19 += state09;
-				state1A += state0A;
-				state1B += state0B;
-				state1C += state0C;
-				state1D += state0D;
-				state1E += state0E;
-				state1F += state0F;
+				x10 += x00;
+				x11 += x01;
+				x12 += x02;
+				x13 += x03;
+				x14 += x04;
+				x15 += x05;
+				x16 += x06;
+				x17 += x07;
+				x18 += x08;
+				x19 += x09;
+				x1A += x0A;
+				x1B += x0B;
+				x1C += x0C;
+				x1D += x0D;
+				x1E += x0E;
+				x1F += x0F;
 
-				y4 = state00;
-				y5 = state01;
-				y6 = state02;
-				y7 = state03;
-				y0 = state04;
-				y1 = state05;
-				y2 = state06;
-				y3 = state07;
-				yC = state08;
-				yD = state09;
-				yE = state0A;
-				yF = state0B;
-				y8 = state0C;
-				y9 = state0D;
-				yA = state0E;
-				yB = state0F;
+				y4 = x00;
+				y5 = x01;
+				y6 = x02;
+				y7 = x03;
+				y0 = x04;
+				y1 = x05;
+				y2 = x06;
+				y3 = x07;
+				yC = x08;
+				yD = x09;
+				yE = x0A;
+				yF = x0B;
+				y8 = x0C;
+				y9 = x0D;
+				yA = x0E;
+				yB = x0F;
 
-				state00 = ((y0 << 11) | (y0 >> (32 - 11))); // ROTATE(y0, 11);
-				state01 = ((y1 << 11) | (y1 >> (32 - 11))); // ROTATE(y1, 11);
-				state02 = ((y2 << 11) | (y2 >> (32 - 11))); // ROTATE(y2, 11);
-				state03 = ((y3 << 11) | (y3 >> (32 - 11))); // ROTATE(y3, 11);
-				state04 = ((y4 << 11) | (y4 >> (32 - 11))); // ROTATE(y4, 11);
-				state05 = ((y5 << 11) | (y5 >> (32 - 11))); // ROTATE(y5, 11);
-				state06 = ((y6 << 11) | (y6 >> (32 - 11))); // ROTATE(y6, 11);
-				state07 = ((y7 << 11) | (y7 >> (32 - 11))); // ROTATE(y7, 11);
-				state08 = ((y8 << 11) | (y8 >> (32 - 11))); // ROTATE(y8, 11);
-				state09 = ((y9 << 11) | (y9 >> (32 - 11))); // ROTATE(y9, 11);
-				state0A = ((yA << 11) | (yA >> (32 - 11))); // ROTATE(yA, 11);
-				state0B = ((yB << 11) | (yB >> (32 - 11))); // ROTATE(yB, 11);
-				state0C = ((yC << 11) | (yC >> (32 - 11))); // ROTATE(yC, 11);
-				state0D = ((yD << 11) | (yD >> (32 - 11))); // ROTATE(yD, 11);
-				state0E = ((yE << 11) | (yE >> (32 - 11))); // ROTATE(yE, 11);
-				state0F = ((yF << 11) | (yF >> (32 - 11))); // ROTATE(yF, 11);
+				x00 = ((y0 << 11) | (y0 >> (32 - 11))); // ROTATE(y0, 11);
+				x01 = ((y1 << 11) | (y1 >> (32 - 11))); // ROTATE(y1, 11);
+				x02 = ((y2 << 11) | (y2 >> (32 - 11))); // ROTATE(y2, 11);
+				x03 = ((y3 << 11) | (y3 >> (32 - 11))); // ROTATE(y3, 11);
+				x04 = ((y4 << 11) | (y4 >> (32 - 11))); // ROTATE(y4, 11);
+				x05 = ((y5 << 11) | (y5 >> (32 - 11))); // ROTATE(y5, 11);
+				x06 = ((y6 << 11) | (y6 >> (32 - 11))); // ROTATE(y6, 11);
+				x07 = ((y7 << 11) | (y7 >> (32 - 11))); // ROTATE(y7, 11);
+				x08 = ((y8 << 11) | (y8 >> (32 - 11))); // ROTATE(y8, 11);
+				x09 = ((y9 << 11) | (y9 >> (32 - 11))); // ROTATE(y9, 11);
+				x0A = ((yA << 11) | (yA >> (32 - 11))); // ROTATE(yA, 11);
+				x0B = ((yB << 11) | (yB >> (32 - 11))); // ROTATE(yB, 11);
+				x0C = ((yC << 11) | (yC >> (32 - 11))); // ROTATE(yC, 11);
+				x0D = ((yD << 11) | (yD >> (32 - 11))); // ROTATE(yD, 11);
+				x0E = ((yE << 11) | (yE >> (32 - 11))); // ROTATE(yE, 11);
+				x0F = ((yF << 11) | (yF >> (32 - 11))); // ROTATE(yF, 11);
 
-				state00 ^= state10;
-				state01 ^= state11;
-				state02 ^= state12;
-				state03 ^= state13;
-				state04 ^= state14;
-				state05 ^= state15;
-				state06 ^= state16;
-				state07 ^= state17;
-				state08 ^= state18;
-				state09 ^= state19;
-				state0A ^= state1A;
-				state0B ^= state1B;
-				state0C ^= state1C;
-				state0D ^= state1D;
-				state0E ^= state1E;
-				state0F ^= state1F;
+				x00 ^= x10;
+				x01 ^= x11;
+				x02 ^= x12;
+				x03 ^= x13;
+				x04 ^= x14;
+				x05 ^= x15;
+				x06 ^= x16;
+				x07 ^= x17;
+				x08 ^= x18;
+				x09 ^= x19;
+				x0A ^= x1A;
+				x0B ^= x1B;
+				x0C ^= x1C;
+				x0D ^= x1D;
+				x0E ^= x1E;
+				x0F ^= x1F;
 
-				y1 = state10;
-				y0 = state11;
-				y3 = state12;
-				y2 = state13;
-				y5 = state14;
-				y4 = state15;
-				y7 = state16;
-				y6 = state17;
-				y9 = state18;
-				y8 = state19;
-				yB = state1A;
-				yA = state1B;
-				yD = state1C;
-				yC = state1D;
-				yE = state1E;
-				yF = state1F;
+				y1 = x10;
+				y0 = x11;
+				y3 = x12;
+				y2 = x13;
+				y5 = x14;
+				y4 = x15;
+				y7 = x16;
+				y6 = x17;
+				y9 = x18;
+				y8 = x19;
+				yB = x1A;
+				yA = x1B;
+				yD = x1C;
+				yC = x1D;
+				yE = x1E;
+				yF = x1F;
 
-				state10 = y0;
-				state11 = y1;
-				state12 = y2;
-				state13 = y3;
-				state14 = y4;
-				state15 = y5;
-				state16 = y6;
-				state17 = y7;
-				state18 = y8;
-				state19 = y9;
-				state1A = yA;
-				state1B = yB;
-				state1C = yC;
-				state1D = yD;
-				state1E = yE;
-				state1F = yF;
+				x10 = y0;
+				x11 = y1;
+				x12 = y2;
+				x13 = y3;
+				x14 = y4;
+				x15 = y5;
+				x16 = y6;
+				x17 = y7;
+				x18 = y8;
+				x19 = y9;
+				x1A = yA;
+				x1B = yB;
+				x1C = yC;
+				x1D = yD;
+				x1E = yE;
+				x1F = yF;
 			}
-
-			state[0] = state00;
-			state[1] = state01;
-			state[2] = state02;
-			state[3] = state03;
-			state[4] = state04;
-			state[5] = state05;
-			state[6] = state06;
-			state[7] = state07;
-			state[8] = state08;
-			state[9] = state09;
-			state[10] = state0A;
-			state[11] = state0B;
-			state[12] = state0C;
-			state[13] = state0D;
-			state[14] = state0E;
-			state[15] = state0F;
-			state[16] = state10;
-			state[17] = state11;
-			state[18] = state12;
-			state[19] = state13;
-			state[20] = state14;
-			state[21] = state15;
-			state[22] = state16;
-			state[23] = state17;
-			state[24] = state18;
-			state[25] = state19;
-			state[26] = state1A;
-			state[27] = state1B;
-			state[28] = state1C;
-			state[29] = state1D;
-			state[30] = state1E;
-			state[31] = state1F;
 		}
 
 	}
